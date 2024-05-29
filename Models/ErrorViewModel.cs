dc86040c9d00fd818565acba1e4a9ddeeacc82d9
Kanter Alon <95320323@est.ort.edu.ar>
2024-05-29 08:19:36 -0300

    Init MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP04_Paquetes_Tur_sticos.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
